5c7e4dbd359a6047d927a6ed91eaa7b06991092f
careverca <airmincgn@gmail.com>
2022-09-18 14:17:16 +0300

    autocommit

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
 // Querying elements
-let likes = document.querySelectorAll(".element__like");
+//let likes = document.querySelectorAll(".element__like");
 let editForm = document.querySelector(".popup");
 let editBtn = document.querySelector(".profile__edit");
 let closeBtn = editForm.querySelector(".popup__close");
 let submitBtn = editForm.querySelector(".popup__submit");
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__job");
+let inputName = editForm.querySelector(".popup__input_type_name");
+let inputJob = editForm.querySelector(".popup__input_type_job");
+
+function openPopup() {
+    togglePopup()
+    inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;
+}
+
+function togglePopup() {
+    editForm.classList.toggle('popup_opened');
+}
 
 // Handling "like buttons"
-likes.forEach(like => {
+/*likes.forEach(like => {
     like.addEventListener("click", function likeToggle() {
         like.classList.toggle('element__like_active');
     })
-} )
+} )*/
 
 // Handling "edit button"
-editBtn.addEventListener("click", function openPopup() {
-    editForm.classList.toggle('popup_opened');
-    let profileName = document.querySelector(".profile__name");
-    let profileJob = document.querySelector(".profile__job");
-    editForm.querySelector(".popup__input_name").value = profileName.innerHTML;
-    editForm.querySelector(".popup__input_job").value = profileJob.innerHTML;
-})
+editBtn.addEventListener("click", openPopup);
 
 // Handling "close button"
-closeBtn.addEventListener("click", function closePopup() {
-    editForm.classList.toggle('popup_opened');
-})
+closeBtn.addEventListener("click", togglePopup);
 
 // Handling "submit button"
 submitBtn.addEventListener("click", function submit(evt){
-evt.preventDefault();
-let inputName = editForm.querySelector(".popup__input_name");
-let inputJob = editForm.querySelector(".popup__input_job");
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__job");
-profileName.innerHTML = inputName.value;
-profileJob.innerHTML = inputJob.value;
-editForm.classList.toggle('popup_opened');
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileJob.textContent = inputJob.value;
+    togglePopup();
 })
